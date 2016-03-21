dcc79cc1ac2bba49fb39ed7e6a9be91e53dd5742
allansene <allsene@dcc.ufmg.br>
2016-03-21 15:02:38 -0300

    Update de pacotes.

--- DCCStore.MVC/App_Start/IdentityConfig.cs
+++ DCCStore.MVC/App_Start/IdentityConfig.cs
@@ -1,6 +1,5 @@
 ﻿using DCCStore.MVC.Models;
 using DCCStore.MVC.Services;
-using DCCStore.Services.Services;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
@@ -12,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace DCCStore.MVC
 {
-       
+
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
