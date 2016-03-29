4a7cdeada7d1854d2d99a23616ab13988a22d7fb
allansene <allsene@dcc.ufmg.br>
2016-03-29 13:55:38 -0300

    Revert do Account controller antigo.

--- DCCScore.MVC/App_Start/WebApiConfig.cs
+++ DCCScore.MVC/App_Start/WebApiConfig.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
 
